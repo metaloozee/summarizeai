32f28c624ba67f74081a9c19757cdb8f2f83c087
metaloozee <ayanparkar123@gmail.com>
2025-02-06 23:09:14 +0530

    feat: upgraded next and changed the dir structure

--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default async function IndexPage() {
-    return <h1>Hello World</h1>
-}
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
             {
